9e930de99ab80a98a422c970531dd66fa08d4d95
Daniel Sarreira <danisarreira@live.com.pt>
2018-12-13 11:39:28 +0000

    RecycleView da Lista de Jogos fragmentInicial e outras algumas atualizações

--- app/src/main/java/pt/ipleiria/estg/dei/amsi/myscores/fragmentCiriarEquipaA.java
+++ app/src/main/java/pt/ipleiria/estg/dei/amsi/myscores/fragmentCiriarEquipaA.java
@@ -1,18 +1,26 @@
 package pt.ipleiria.estg.dei.amsi.myscores;
 
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class fragmentCiriarEquipaA extends Fragment {
-
 
     public fragmentCiriarEquipaA() {
         // Required empty public constructor
@@ -24,6 +32,7 @@ public class fragmentCiriarEquipaA extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_criar_equipa_a, container, false);
+
         return view;
     }
 
---
+++ app/src/main/java/pt/ipleiria/estg/dei/amsi/myscores/fragmentEditarPerfil.java
@@ -0,0 +1,39 @@
+package pt.ipleiria.estg.dei.amsi.myscores;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class fragmentEditarPerfil extends Fragment {
+
+
+    public fragmentEditarPerfil() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_editar_perfil, container, false);
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(NavegationDrawerConstants.TAG_EDITARPERFIL);
+    }
+
+}
